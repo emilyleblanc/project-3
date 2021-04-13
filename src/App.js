da812e58ff03fe3a10a4499a6bf9ac11689f3e63
Emily LeBlanc <emily.leblanc88@gmail.com>
2021-04-13 16:20:49 -0400

    shopping cart functions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useState, useEffect } from 'react'
 function App() {
   const [inventory, setInventory] = useState([]);
   const [shoppingCart, setShoppingCart] = useState({});
- 
+
 
 
   // On mount the inventory of yarn product data from firebase is stored in an array called yarnBag
@@ -30,33 +30,85 @@ function App() {
 
   // handleAddInventoryToCart is fired when user presses "Add to Cart"
   // this function stores the clicked item in a useState function to display product name, price and quantity in the shopping cart section
+  const newCart = { ...shoppingCart };
+
+  console.log('newCart:', newCart)
   
   const handleAddInventoryToCart = (purchase) => {
-    const newCart = { ...shoppingCart };
-    
+   
     if (newCart[purchase.uniqueKey]) {
       const cartItem = newCart[purchase.uniqueKey];
-
       cartItem.quantity = cartItem.quantity + 1;
       cartItem.total = cartItem.total + purchase.products.price;
-
-      // cartItem.inventoryItem.products.price = cartItem.inventoryItem.products.price + purchase.products.price;
+      // get nodeList of table elements
+      
     } else {
       newCart[purchase.uniqueKey] = {
         quantity: 1,
         inventoryItem: purchase,
         total: purchase.products.price,
+        remove: function() {
+          return this.quantity - 1;
+        },
       }
     }
+    
+    const totalsAddedTogether = document.querySelectorAll('#totalPurchases');
+    
+     const array = []
+     // turn nodeList into an array and iterate through the array 
+    Array.from(totalsAddedTogether).forEach((total)=>{
+     //  collect the integer values of each total in the shoppingCart
+      const parsedTotal = parseInt(total.textContent)
+     //  push to the array
+      array.push(parsedTotal);
+    });
+ 
+   //  determine the sum of that array
+     const sum = array.reduce(function(a, b, c){
+     return a + b + c;
+     }, 0);
 
+     console.log(sum)
+    
     setShoppingCart(newCart)
   }
-      
-      
+
+  // const handleRemoveFromCart = () => {
+  //   console.log('clicked')
+  //   console.log(newCart)
+  //   // return newCart[purchase.quantity];
+  // };
+
+  const slideOutMenu = () => {
+    document.getElementById('shoppingCart').classList.remove('shoppingCart');
+    document.getElementById('shoppingCart').classList.add('openMenu');
+  }
+    
+
+  const closeMenu = () => {
+    document.getElementById('shoppingCart').classList.remove('openMenu');
+    document.getElementById('shoppingCart').classList.add('shoppingCart');
+
+  }
+
+  
+   
+
 
   return (
 
     <div className="App">
+      <nav>
+        <ul>
+          <li><a href="#">
+            <i class="fas fa-shopping-bag"
+            onClick = {slideOutMenu}></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       {/* START OF HEADER */}
       <header className="wrapper">
         <h1>Knit <span>+</span> Pearl</h1>
@@ -87,7 +139,11 @@ function App() {
 
         {/* START OF SHOPPING CART SECTION */}
 
-        <section className={'shoppingCart wrapper'}>
+        <section 
+        className={'shoppingCart wrapper'}
+        id={'shoppingCart'}
+        onClick = {closeMenu}
+        >
           <table>
               <tr>
                 <th>Purchase</th>
@@ -99,28 +155,30 @@ function App() {
 
               {
                 Object.values(shoppingCart).map((item) => {
-                  const { quantity, inventoryItem,total } = item;
+                  const { quantity, inventoryItem, total, remove} = item;
                   return <DisplayShoppingCart
                     purchaseName={inventoryItem.products.product_name}
                     quantity={quantity}
                     purchasePrice={total}
+                    removeFunction = {remove}
                   />
                 })
               }
               <tr>
                 <th>Total:</th>
-                <td>{`this will be the total`}</td>
+                <td></td>
 
               </tr>
 
           
           </table>
+          <button>Check Out</button>
         </section>
       </main>
 
       {/* START OF FOOTER */}
-
       <footer>created at <span><a href="https://junocollege.com/">Juno College</a></span> by Emily</footer>
+
     </div>
   );
 }
--- src/DisplayShoppingCart.js
+++ src/DisplayShoppingCart.js
@@ -3,11 +3,21 @@
 // SHOPPING CART COMPONENT IS THE INFORMATION DISPLAYED IN THE SHOPPING CART SECTION WHEN ITEM IS SELECTED FOR PURCHASE. IT IS REPRESENTED IN A TABLE 
 
 const ShoppingCart = (props) => {
+
+//   const handleRemoveFromCart = () => {
+//     console.log('clicked')
+
+//     // If item already exists in the shopping cart
+//     // lower the quantity by 1
+//     // lower the purchase price by the items price
+
+//     // Else remove from shopping cart object
     return(
             <tr>
                 <td>{props.purchaseName}</td>
                 <td>{props.quantity}</td>
-                <td>{`$${props.purchasePrice}`}</td>
+                <td id="totalPurchases">{props.purchasePrice}</td>
+                <button onClick ={props.remove}>remove</button>
             </tr>
     )
 };
